ceaa602290dc1beb2f5c0cdb2382312a974cc966
Lo Van Long <longlv@v-auto.com.vn>
2021-05-17 11:10:51 +0700

    first commit

--- components/avatar.js
+++ components/avatar.js
@@ -10,7 +10,8 @@ export default function Avatar({ author }) {
       {author && (
         <div className="flex items-center">
           <img
-            src={author.avatar.url}
+            // src={author.avatar.url}
+            src="https://admin.tiemchunggiongvina.com/wp-content/uploads/nsl_avatars/0d0f78ea41a920867ea6b4e6daed9ddb.png"
             className="w-12 h-12 rounded-full mr-4"
             alt={name}
           />
